f54f2396dd144256d25f19e223843b05c07d9591
Kshitiz VIkram Singh <kshitizvikramsingh123@gmail.com>
2024-06-30 01:26:41 +0530

    added a few imp files for appspec

---
+++ after_install.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+mkdir /home/ec2-user/shaadibazar
+cd /home/ec2-user/shaadibazar
+
+npm install
---
+++ application_start.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+sudo npx pm2 kill
+
+sudo npx pm2 start /home/ec2-user/shaadibazar/app.js
